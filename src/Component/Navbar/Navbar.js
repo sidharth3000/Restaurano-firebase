52a0f81f526e55fb5882bf5bb59951c9f4c7df17
sidharth <201952234@iiitvadodara.ac.in>
2021-03-16 23:40:44 +0530

    home init

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import Logo from '../../Assets/logo.png'
+import './Navbar.css'
+
+class Navbar extends Component {
+    render () {
+    
+        return(
+            <div className='navbar_container'>
+                <div className="navbar_logo">
+                    <img className='navbar_img' src={Logo} alt="logo" />
+                    
+                </div>
+                <div className='navbar_name'>RESTURANO</div>
+               <div className="home">Home</div>
+               <div className="orders">Orders</div>
+               <div className="reservation">Reservation</div>
+               <div className="feed">Feedback</div>
+            </div>
+        ) ;
+    }
+}
+
+
+
+export default Navbar;
---
+++ src/Container/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Navbar from '../../Component/Navbar/Navbar';
+
+
+class Home extends Component {
+    render () {
+    
+        return(
+            <div>
+                <Navbar/>
+            </div>
+        ) ;
+    }
+}
+
+
+
+export default Home;
